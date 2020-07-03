ebb137901fe459a2500dc2423e786b4b3c48fcb5
Pranjali Bajpai <2018eeb1243@iitrpr.ac.in>
2020-07-03 13:22:30 +0530

    New design

--- script.js
+++ script.js
@@ -1,18 +1,12 @@
-function ChooseSymbol()
-{
-  document.getElementById('player').innerHTML="Choose your symbol :";
-  var first=document.getElementsByClassName('pbutton')[0];
-  var second=document.getElementsByClassName('pbutton')[1];
-  first.innerHTML="X";
-  second.innerHTML="O";
-  first.onclick=function(){ChooseLevel};
-  second.onclick=function(){ChooseLevel};
+function ChoosePlayer(player){
+    
 }
-function ChooseLevel()
-{
-  document.getElementById('player').innerHTML="Choose level :";
-  var first=document.getElementsByClassName('pbutton')[0];
-  var second=document.getElementsByClassName('pbutton')[1];
-  first.innerHTML="Easy";
-  second.innerHTML="Hard";
+function ChooseSymbol(cross){
+
+}
+function ChooseLevel(easy){
+
+}
+function StartGame(){
+
 }
