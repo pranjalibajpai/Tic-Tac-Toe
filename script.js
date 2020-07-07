eb04a7f38eaec2e5c399085e5c90d04979196257
Pranjali Bajpai <2018eeb1243@iitrpr.ac.in>
2020-07-07 18:39:09 +0530

    2 Player game ready!

--- script.js
+++ script.js
@@ -36,7 +36,13 @@ function ChooseSymbol(symbol){
         p2_symb='X';
     }
     document.getElementById("choose_symbol").style.display = 'none';
-    document.getElementById("choose_level").style.display = 'block';
+	if(computer){
+		document.getElementById("choose_level").style.display = 'block';
+	}
+    else{
+		document.getElementById("choose_level").style.display = 'none';
+		document.getElementById("start_button").style.display = 'block';
+	}
 }
 function ChooseLevel(level){
     if(!level){
@@ -47,12 +53,21 @@ function ChooseLevel(level){
 }
 Play();
 function Play(){
+
     initboard = Array.from(Array(9).keys());
     for(let i=0;i<boxes.length;i++){
         boxes[i].innerText = '';
         boxes[i].style.opacity = 0.6;
         boxes[i].addEventListener('click', getClickID, false);
     }
+}
+function Update(){
+	p1_score=0;
+	p2_score=0;
+	tie=0;
+	document.getElementById("p1").innerText= p1_score;
+	document.getElementById("tie").innerText = tie;
+	document.getElementById("p2").innerText = p2_score;
 }
 function toggle_player(){
     current_player = current_player === p1_symb ? p2_symb : p1_symb;
@@ -61,15 +76,27 @@ function toggle_player(){
 function display_winner(game_won){
     let elem;
 	let time_step = 200;
+	if(p1_symb === game_won.player)
+	p1_score++;
+	else {
+		p2_score++;
+	}
+	document.getElementById("p1").innerText= p1_score;
+	document.getElementById("p2").innerText = p2_score;
 	for( let i = 0; i < game_won.combo.length; i++ ){
-
 		setTimeout(() => {elem = game_won.combo[i];
 		document.getElementById(elem).style.opacity = 1;
 		document.getElementById(elem).style.webkitAnimationName = 'glow' ;
 		document.getElementById(elem).style.webkitAnimationDuration = '2s';
 		console.log('Delay')}, i*time_step);
 	}
-	setTimeout(() => {alert('Player '+game_won.player+' won!!');}, time_step*4);
+	//let msg = document.getElementById("message");
+	document.getElementById("message").style.display = 'block';
+	if(game_won.player === p1_symb)
+	document.getElementById("message").innerText = "Hurray !  Player 1  won...";
+	else
+	document.getElementById("message").innerText = "Hurray !  Player 2  won...";
+	//setTimeout(() => {alert('Player '+game_won.player+' won!!');}, time_step*4);
 	for (var i = 0; i < boxes.length; i++) {
 		boxes[i].removeEventListener('click', getClickID, false);
 	}
@@ -88,16 +115,43 @@ function checkGameWon(board, player){
         if(win.every((elem)=> result.indexOf(elem) > -1)){
             game_won = {index:i, player:player, combo:win};
             display_winner(game_won);
+			return true;
         }
     }
+	return false;
 }
-
+function checkGameDraw(board){
+	let time_step = 200;
+	var draw = true;
+	for(let i=0; i<board.length; i++){
+		if(board[i] === 'X' || board[i] === 'O');
+		else{
+			draw = false;
+			break;
+		}
+	}
+	if(draw){
+		tie++;
+		document.getElementById("tie").innerText = tie;
+		document.getElementById("message").style.display = 'block';
+		document.getElementById("message").innerText = "Ugh !  It was a draw...";
+		//setTimeout(() => {alert("Ugh! It's a Tie");}, time_step*4);
+		for (let i = 0; i < boxes.length; i++) {
+			boxes[i].removeEventListener('click', getClickID, false);
+		}
+	}
+}
 function fillbox(boxID, player){
     if(initboard[boxID] != p1_symb && initboard[boxID] != p2_symb){
         initboard[boxID] = player;
         boxes[boxID].innerText = player;
         toggle_player();
-        checkGameWon(initboard, player);
+		var check = checkGameWon(initboard, player);
+		//console.log(check);
+		if(!check)
+		checkGameDraw(initboard);
+        //if(!checkGameWon(initboard, player))
+		//checkGameDraw(initboard);
     }
 }
 function getClickID(box){
@@ -112,8 +166,15 @@ function StartGame(){
 
 }
 function Back(){
+	Update();
+	Play();
     document.getElementsByClassName("start_menu")[0].style.display = 'block';
     document.getElementById("welcome").style.display = 'block';
     document.getElementById("choose_opponent").style.display = 'block';
     document.getElementsByClassName("start_playing")[0].style.display = 'none';
+}
+function Reset(){
+	Play();
+	current_player = p1_symb;
+	document.getElementById("message").style.display = 'none';
 }
